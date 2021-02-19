0eaa3eb7e7d9203787863c59f4b6d9bfab7cdf31
Robert Mark Edwards II <markedwards@MacBook-Pro.cable.rcn.com>
2021-02-19 17:35:38 -0600

    Springs

---
+++ src/test/java/com/mark/strings/StringsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.strings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
